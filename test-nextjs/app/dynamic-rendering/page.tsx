598bba20423643aadb627db8919f6514d19da28c
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-01 18:20:29 +0900

    書き方修正。フォルダ名でのルート設定確認

---
+++ test-nextjs/app/dynamic-rendering/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Box from '@/components/Box';
+
+const DynamicRenderingPage = () => {
+  return (
+    <Box>DynamicRenderingPage</Box>
+  )
+}
+
+export default DynamicRenderingPage;
+
+
